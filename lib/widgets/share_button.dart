7e48b19f401a1da82eccbd588317fd5614fd9241
Tejas Lolage <tejaslolage@gmail.com>
2023-07-07 17:26:58 +0900

    First iteration of the share button

---
+++ lib/widgets/share_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+class ShareButton extends StatelessWidget {
+  final String url;
+
+  const ShareButton({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  void onPressed() {
+    Share.share(url);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String text = "Share";
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
