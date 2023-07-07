7e85931fa025b0618ebd627952b74198490403b7
Aadeesh11 <asoswal@gmail.com>
2023-07-07 15:01:56 +0530

    cached network image.

---
+++ lib/widgets/article_image.dart
@@ -0,0 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ArticleImage extends StatelessWidget {
+  const ArticleImage(this.url, {super.key});
+  final String url;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      borderOnForeground: true,
+      margin: EdgeInsets.zero,
+      child: CachedNetworkImage(
+        imageUrl: "http://via.placeholder.com/350x150",
+        fit: BoxFit.fill,
+        width: double.infinity,
+        height: url == '' ? 200 : 300,
+        errorWidget: (context, url, error) => const Placeholder(),
+        placeholder: (context, url) => const SizedBox(
+          height: 60,
+          child: Center(child: CircularProgressIndicator.adaptive()),
+        ),
+      ),
+    );
+  }
+}
