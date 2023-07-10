b3510548f7316cfe13d827b43004c1d54fb908f1
Tejas Lolage <tejaslolage@gmail.com>
2023-07-10 17:29:15 +0900

    Revert "Removed redundant internationalization"
    This reverts commit 9091a400d4a88a78c93098caf51dabfb2cf8c8cf.

--- lib/widgets/redirect_to_website.dart
+++ lib/widgets/redirect_to_website.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -10,7 +11,8 @@ class RedirectToWebsite extends StatelessWidget {
   void onPressed() async {
     final Uri urlToBeLaunched = Uri.parse(url);
     if (!await launchUrl(urlToBeLaunched)) {
-      throw Exception('Could not open $urlToBeLaunched');
+      var couldNotOpen = Intl.message('couldNotOpen');
+      throw Exception('$couldNotOpen $urlToBeLaunched');
     }
   }
 
