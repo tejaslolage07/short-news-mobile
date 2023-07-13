d9d9f4f61d053d133e0988fe846192e1c7100e0b
Aadeesh11 <asoswal@gmail.com>
2023-07-13 12:08:44 +0530

    modular code

--- lib/news/view/news_articles_page.dart
+++ lib/news/view/news_articles_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_repository/news_repository.dart';
 import 'package:short_news_mobile/news/bloc/news_articles_bloc.dart';
-import 'package:short_news_mobile/widgets/ArticleList.dart';
+
+import 'news_articles_view.dart';
 
 class NewsArticlesPage extends StatelessWidget {
   const NewsArticlesPage({super.key});
@@ -21,34 +22,6 @@ class NewsArticlesPage extends StatelessWidget {
         ),
         child: const NewsArticlesView(),
       ),
-    );
-  }
-}
-
-class NewsArticlesView extends StatefulWidget {
-  const NewsArticlesView({super.key});
-
-  @override
-  State<NewsArticlesView> createState() => _NewsArticlesViewState();
-}
-
-class _NewsArticlesViewState extends State<NewsArticlesView> {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<NewsArticleBloc, NewsArticlesState>(
-      builder: (context, state) {
-        switch (state.status) {
-          case NewsArticlesStatus.failure:
-            return const Center(child: Text('Failed to fetch news articles'));
-          case NewsArticlesStatus.success:
-            return ArticleList();
-          case NewsArticlesStatus.initial:
-            context.read<NewsArticleBloc>().add(
-                  const NewsArticlesFetch(count: 100),
-                );
-            return const Center(child: CircularProgressIndicator());
-        }
-      },
     );
   }
 }
