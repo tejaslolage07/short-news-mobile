194ed331378b99a8bf3bcfe656e8b8122879f252
Tejas Lolage <tejaslolage@gmail.com>
2023-07-19 10:47:56 +0900

    Updated Themes and used app constants.

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,23 +1,65 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:short_news_mobile/app_constants.dart';
 
 class AppTheme {
   AppTheme();
 
-  static ThemeData get lightTheme {
-    var colorScheme = ColorScheme.fromSwatch(
-      primarySwatch: Colors.deepPurple,
-      accentColor: Colors.teal,
-      brightness: Brightness.light,
+  static NativeTemplateStyle getNativeAdTheme() {
+    bool isDarkMode =
+        WidgetsBinding.instance.platformDispatcher.platformBrightness ==
+            ui.Brightness.dark;
+    Color mainBackgroundColor;
+    Color backgroundColor;
+    Color textColor;
+    if (isDarkMode) {
+      mainBackgroundColor = const Color.fromARGB(255, 48, 48, 48);
+      backgroundColor = const Color.fromARGB(255, 48, 48, 48);
+      textColor = Colors.white;
+    } else {
+      mainBackgroundColor = Colors.white;
+      backgroundColor = Colors.white;
+      textColor = AppConstants.themeColor;
+    }
+    return NativeTemplateStyle(
+      templateType: TemplateType.medium,
+      mainBackgroundColor: mainBackgroundColor,
+      cornerRadius: 10.0,
+      callToActionTextStyle: NativeTemplateTextStyle(
+          textColor: textColor, backgroundColor: backgroundColor, size: 16.0),
+      primaryTextStyle: NativeTemplateTextStyle(
+          textColor: textColor, backgroundColor: backgroundColor, size: 16.0),
+      secondaryTextStyle: NativeTemplateTextStyle(
+          textColor: textColor, backgroundColor: backgroundColor, size: 16.0),
+      tertiaryTextStyle: NativeTemplateTextStyle(
+          textColor: textColor, backgroundColor: backgroundColor, size: 16.0),
     );
+  }
+
+  static ThemeData get lightTheme {
+    final colorScheme =
+        ColorScheme.fromSeed(seedColor: AppConstants.themeColor);
 
     return ThemeData(
       useMaterial3: true,
       colorScheme: colorScheme,
       scaffoldBackgroundColor: Colors.white,
       appBarTheme: AppBarTheme(
-        backgroundColor: colorScheme.primary,
-        foregroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        titleTextStyle: GoogleFonts.ubuntu(
+          fontSize: 25,
+          color: colorScheme.primary,
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: colorScheme.primary,
       ),
       floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: colorScheme.primary,
@@ -25,14 +67,13 @@ class AppTheme {
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.transparent),
-          elevation: MaterialStateProperty.all(1),
-          // foregroundColor: MaterialStateProperty.all(colorScheme.onPrimary),
+          backgroundColor: MaterialStateProperty.all(colorScheme.primary),
+          foregroundColor: MaterialStateProperty.all(colorScheme.onPrimary),
           shape: MaterialStateProperty.all(
             const RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16.0),
-                topRight: Radius.circular(16.0),
+                topLeft: Radius.circular(100),
+                topRight: Radius.circular(100),
               ),
             ),
           ),
@@ -54,23 +95,69 @@ class AppTheme {
           fontWeight: FontWeight.normal,
           color: Colors.black38,
         ),
+        bodyMedium: GoogleFonts.ubuntu(
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+        ),
       ),
     );
   }
 
   static ThemeData get darkTheme {
-    var darkColorScheme = ColorScheme.fromSwatch(
-      primarySwatch: Colors.blueGrey,
-      accentColor: Colors.amber,
-      brightness: Brightness.dark,
-    );
-
-    return ThemeData.dark().copyWith(
+    return ThemeData(
       useMaterial3: true,
-      colorScheme: darkColorScheme,
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: darkColorScheme.primary,
-        foregroundColor: darkColorScheme.onPrimary,
+      scaffoldBackgroundColor: const Color.fromARGB(255, 48, 48, 48),
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        titleTextStyle: GoogleFonts.ubuntu(
+          fontSize: 25,
+        ),
+        backgroundColor: const Color.fromARGB(255, 66, 66, 66),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Color.fromARGB(255, 48, 48, 48),
+        foregroundColor: Colors.white,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all(const Color.fromARGB(255, 67, 69, 68)),
+          shape: MaterialStateProperty.all(
+            const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(100),
+                topRight: Radius.circular(100),
+              ),
+            ),
+          ),
+        ),
+      ),
+      textTheme: TextTheme(
+        titleSmall: GoogleFonts.ubuntu(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+        bodyLarge: GoogleFonts.ubuntu(
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+          color: const Color.fromARGB(255, 225, 225, 225),
+        ),
+        bodySmall: GoogleFonts.ubuntu(
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: Colors.grey,
+        ),
+        bodyMedium: GoogleFonts.ubuntu(
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+        ),
       ),
     );
   }
