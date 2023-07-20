8b3dd59b0da2fac7ba575427a53a69ef15dce7c7
Tejas Lolage <tejaslolage@gmail.com>
2023-07-20 13:21:44 +0900

    Added styling in app-bar file

--- lib/news/widgets/ news_articles_app_bar.dart
+++ lib/news/widgets/ news_articles_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:short_news_mobile/news/bloc/news_articles_bloc.dart';
 
 class ArticlesAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -11,8 +12,6 @@ class ArticlesAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     AppLocalizations localizations = AppLocalizations.of(context)!;
     return AppBar(
-      centerTitle: true,
-      elevation: 0,
       title: Text(localizations.articleAppBarTitle),
       actions: [
         IconButton(
@@ -23,6 +22,18 @@ class ArticlesAppBar extends StatelessWidget implements PreferredSizeWidget {
           },
         ),
       ],
+      centerTitle: true,
+      elevation: 0,
+      surfaceTintColor: Colors.white,
+      iconTheme: IconThemeData(
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      titleTextStyle: GoogleFonts.ubuntu(
+        fontSize: 25,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      backgroundColor: Colors.white,
+      foregroundColor: Theme.of(context).colorScheme.primary,
     );
   }
 
