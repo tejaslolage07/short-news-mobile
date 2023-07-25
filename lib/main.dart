01f98008bb6c85ba4c74992fb7609a10956be010
Aadeesh11 <asoswal@gmail.com>
2023-07-25 14:04:30 +0530

    Revert "Merge remote-tracking branch 'origin/main' into image-widget"
    This reverts commit 9e3bc661937b9bc7dd881ed915ed2006fe2f27a7, reversing
    changes made to 0baf8c479387ca741dcc284946dd6313a0f58536.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:news_repository/news_repository.dart';
 import 'package:short_news_mobile/app_bloc_observer.dart';
-
+import 'package:timeago/timeago.dart' as timeago;
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = const AppBlocObserver();
+  timeago.setLocaleMessages('ja', timeago.JaMessages());
 
   runApp(
     ShortNewsApp(
