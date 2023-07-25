aa95022935a925f1c0cd5219605f28527466a0f8
Tejas Lolage <tejaslolage@gmail.com>
2023-07-25 15:48:17 +0900

    Refactored app-bar

--- lib/news/widgets/ news_articles_app_bar.dart
+++ lib/news/widgets/ news_articles_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:short_news_mobile/google_ads/bloc/google_ads_bloc.dart';
 import 'package:short_news_mobile/news/bloc/news_articles_bloc.dart';
 
 class ArticlesAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -11,8 +12,12 @@ class ArticlesAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     AppLocalizations localizations = AppLocalizations.of(context)!;
     return AppBar(
+      surfaceTintColor: Colors.white,
       centerTitle: true,
       elevation: 0,
+      titleTextStyle: Theme.of(context).textTheme.titleLarge,
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      foregroundColor: Theme.of(context).colorScheme.onPrimary,
       title: Text(localizations.articleAppBarTitle),
       actions: [
         IconButton(
