0e02f155b838bb4335e9cf752aa2b619f9f9c67c
Tejas Lolage <tejaslolage@gmail.com>
2023-07-25 16:27:21 +0900

    Refactored app constants file

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppConstants {
-  static const int articlesToFetchInAPage = 100;
-  static const int getAdAfterEveryXArticles = 8;
   static const Color lightThemeColor = Colors.pink;
   static const Color darkThemeColor = Color.fromARGB(255, 66, 66, 66);
 }
